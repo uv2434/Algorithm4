6021a31bf77b82d7fbfbcd3d25b3d63519b40091
uv2434 <uv2434@gmail.com>
2021-11-25 11:46:13 +0800

    [author] YJ 第二章第二单元相关习题

--- src/main/java/it/chapter1/section2/P_01_02_18.java
+++ src/main/java/it/chapter1/section2/P_01_02_18.java
@@ -1,6 +1,8 @@
 package it.chapter1.section2;
 
 
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * 1.2.18　 累加器的方差。以下代码为 Accumulator 类添加了 var() 和 stddev() 方法，它们计算了
  * addDataValue() 方法的参数的方差和标准差，验证这段代码。
@@ -28,13 +30,19 @@ public class P_01_02_18 {
 
     public static void main(String[] args) {
 
+        Accumulator accumulator = new Accumulator();
+        for (int i = 0; i < 10000; i++) {
+            accumulator.addDataValue(StdRandom.random());
+        }
+        System.out.println("result:" + accumulator);
 
     }
 
-    public class Accumulator {
+    public static class Accumulator {
         private double m;
         private double s;
         private int N;
+
 
         public void addDataValue(double x) {
             N++;
@@ -52,6 +60,15 @@ public class P_01_02_18 {
 
         public double stddev() {
             return Math.sqrt(this.var());
+        }
+
+        @Override
+        public String toString() {
+            return "Accumulator{" +
+                    "m=" + m +
+                    ", s=" + s +
+                    ", N=" + N +
+                    '}';
         }
     }
 }
